75d3f1dba6082a3826c0eb0f67b97bca5597e19a
Reiya24 <reiya2307@gmai.com>
2024-04-19 16:01:58 +0700

    chore: add ingress

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="Testing argocd, SHOULD BE CHANGED 1.1" />
+      <HelloWorld msg="Testing argocd, SHOULD BE CHANGED 1.4" />
     </div>
   </header>
 
